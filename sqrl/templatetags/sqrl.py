dd81b4573200ba4182e4bda2a9defb66a5d01661
Miroslav Shubernetskiy <miroslav@miki725.com>
2015-03-15 21:17:06 -0700

    added sqrl_qr_image_url() template tag

--- sqrl/templatetags/sqrl.py
+++ sqrl/templatetags/sqrl.py
@@ -2,6 +2,8 @@
 from __future__ import print_function, unicode_literals
 
 from django import template
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import urlencode
 
 from ..sqrl import SQRLInitialization
 
@@ -10,5 +12,10 @@ register = template.Library()
 
 
 @register.assignment_tag(takes_context=True)
-def sqrl_nut(context):
+def sqrl(context):
     return SQRLInitialization(context['request'])
+
+
+@register.simple_tag
+def sqrl_qr_image_url(sqrl):
+    return '{}?url={}'.format(reverse('sqrl:qr-image'), urlencode(sqrl.url))
--- sqrl/urls.py
+++ sqrl/urls.py
@@ -5,24 +5,18 @@ from django.conf.urls import patterns, url
 
 from .views import (
     SQRLAuthView,
-    SQRLCheckView,
+    SQRLCompleteRegistrationView,
     SQRLLoginView,
     SQRLQRGeneratorView,
+    SQRLStatusView,
 )
 
 
 urlpatterns = patterns(
     '',
-    url(r'login/$',
-        SQRLLoginView.as_view(),
-        name='login'),
-    url(r'^qr/$',
-        SQRLQRGeneratorView.as_view(),
-        name='qr-image'),
-    url(r'^check/$',
-        SQRLCheckView.as_view(),
-        name='auth'),
-    url(r'^auth/$',
-        SQRLAuthView.as_view(),
-        name='auth'),
+    url(r'login/$', SQRLLoginView.as_view(), name='login'),
+    url(r'^qr/$', SQRLQRGeneratorView.as_view(), name='qr-image'),
+    url(r'^auth/$', SQRLAuthView.as_view(), name='auth'),
+    url(r'^status/$', SQRLStatusView.as_view(), name='status'),
+    url(r'^register/$', SQRLCompleteRegistrationView.as_view(), name='complete-registration'),
 )
