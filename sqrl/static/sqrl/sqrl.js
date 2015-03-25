01dba7cdd47f5f10acfd3e75b775b125b62ed3f2
Miroslav Shubernetskiy <miroslav@miki725.com>
2015-03-24 20:33:48 -0700

    added copy of jquery to repository for easier offline local development

--- sqrl/static/sqrl/sqrl.js
+++ sqrl/static/sqrl/sqrl.js
@@ -2,7 +2,6 @@ $(function() {
   var sqrl_timeout = setInterval(function() {
     $.post(SQRL_CHECK_URL)
       .done(function(data) {
-        console.log(data);
         if (data.is_logged_in === true || data.redirect_to !== undefined) {
           clearTimeout(sqrl_timeout);
           if (data.redirect_to !== undefined) {
