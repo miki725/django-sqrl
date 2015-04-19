c881232d471b6bbe4533e2b4ea7e436ef42a633d
Miroslav Shubernetskiy <miroslav@miki725.com>
2015-04-18 22:05:09 -0400

    falling back to raw XHR when jquery is not found

--- sqrl/static/sqrl/sqrl.js
+++ sqrl/static/sqrl/sqrl.js
@@ -1,16 +1,57 @@
-$(function() {
-  var sqrl_timeout = setInterval(function() {
-    $.post(SQRL_CHECK_URL)
-      .done(function(data) {
-        if (data.is_logged_in === true || data.redirect_to !== undefined) {
-          clearTimeout(sqrl_timeout);
-          if (data.redirect_to !== undefined) {
-            window.location.href = data.redirect_to;
+'use strict';
+
+(function() {
+  if (window.jQuery !== undefined) {
+    // jQuery API Ajax is much more user-friendly
+    // and handles browsers much better
+    $(function() {
+      var sqrl_timeout = setInterval(function() {
+        $.post(SQRL_CHECK_URL)
+          .done(function(data) {
+            if (data.is_logged_in === true || data.redirect_to !== undefined) {
+              clearTimeout(sqrl_timeout);
+              if (data.redirect_to !== undefined) {
+                window.location.href = data.redirect_to;
+              }
+            }
+          })
+          .fail(function() {
+            clearInterval(sqrl_timeout);
+          });
+      }, 1500);
+    });
+
+  } else {
+    // if jQuery is not found, then fallback to raw XHR use
+    // which is not as reliable but works in most cases
+    window.onload = function() {
+      var sqrl_timeout = setInterval(function() {
+        var request = new XMLHttpRequest();
+        request.open('POST', SQRL_CHECK_URL, false);
+        request.onreadystatechange = handleStateChange;
+
+        function handleStateChange() {
+          if (request.readyState === 4) {
+            if (this.status === 200) {
+              var data = JSON.parse(this.responseText);
+              if (data.is_logged_in === true || data.redirect_to !== undefined) {
+                clearTimeout(sqrl_timeout);
+                if (data.redirect_to !== undefined) {
+                  window.location.href = data.redirect_to;
+                }
+              }
+            } else {
+              clearInterval(sqrl_timeout);
+            }
           }
         }
-      })
-      .fail(function() {
-        clearInterval(sqrl_timeout);
-      });
-  }, 1500);
-});
+
+        try {
+          request.send(null);
+        } catch (exception) {
+          clearInterval(sqrl_timeout);
+        }
+      }, 1500);
+    }
+  }
+})();
