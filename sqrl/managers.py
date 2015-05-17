84007631c9f246dafd78ed15b3341f55e140c24d
Miroslav Shubernetskiy <miroslav@miki725.com>
2015-05-17 17:25:03 -0400

    added docs to managers and models

--- sqrl/managers.py
+++ sqrl/managers.py
@@ -4,7 +4,23 @@ from __future__ import print_function, unicode_literals
 from django.db import models
 
 
-class NutManager(models.Manager):
+class SQRLNutManager(models.Manager):
+    """
+    Customer :obj:`.models.SQRLNut` model manager.
+    """
+
     def replace_or_create(self, session_key, **kwargs):
+        """
+        This method creates new :obj:`.models.SQRLNut` with given parameters.
+
+        If nut already exists, it removes it before creating new nut.
+
+        Parameters
+        ----------
+        session_key : str
+            Key of the session. All nuts with matching session will be removed.
+        **kwargs
+            Kwargs which will be used to create new :obj:`.models.SQRLNut`
+        """
         self.get_queryset().filter(session_key=session_key).delete()
         return self.create(session_key=session_key, **kwargs)
--- sqrl/models.py
+++ sqrl/models.py
@@ -6,17 +6,47 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 from .crypto import generate_randomness
-from .managers import NutManager
+from .managers import SQRLNutManager
 
 
 class SQRLIdentity(models.Model):
     """
+    SQRL identity associated with a user.
+
+    This model stores all necessary for SQRL to complete SQRL transactions
+    and return any data to the client when necessary.
+
+    The reason this is a standalone model vs lets say extending Django User model
+    is because if added to the user model, each row for the user table is forced
+    to allocate space to store SQRL information which might not be desired.
+    By using a dedicated column, it allows to use SQRL only for users who use it.
+    Also this makes Django-SQRL more modular so that it is easier integrated with
+    existing projects.
+
     Attributes
     ----------
+    public_key : str
+        Base64 encoded public key of per-site user's SQRL public-private key pair.
+        This key is used to verify users signature signing SQRL transaction
+        including server generated random nut using users private key.
+    server_unlock_key : str
+        Base64 encoded server unlock key which is a public unlock key sent to client
+        which client can use to generate urs (unlock request signature) signature which
+        server can validate using vuk.
+        More information can be found at https://www.grc.com/sqrl/idlock.htm.
+    verify_unlock_key : str
+        Base64 encoded verify unlock key which is a key stored by server which is used
+        to validate urs (unlock request signature) signatures. This key is not sent to user.
+        More information can be found at https://www.grc.com/sqrl/idlock.htm.
+    is_enabled : bool
+        Boolean indicating whether user can authenticate using SQRL.
     in_only_sqrl : bool
-        Boolean indicating that only SQRL should be allowed to authenticate user
+        Boolean indicating that only SQRL should be allowed to authenticate user.
+        When enabled via flag in SQRL client requests, this should disable all other
+        methods of authentication such as username/password.
     """
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='sqrl_identity')
+    """Foreign key to Django's auth ``User`` object for whom this SQRL identity belongs to."""
 
     public_key = models.CharField(
         max_length=43, db_index=True, unique=True,
@@ -59,17 +89,75 @@ class SQRLIdentity(models.Model):
 @python_2_unicode_compatible
 class SQRLNut(models.Model):
     """
+    Model for storing temporary state for SQRL transactions.
+
+    This model by SQRL protocol is not strictly required.
+    Here is the reasoning for it though:
+
+    SQRL protocol requires couple of things:
+
+    1. Each SQRL interaction must use random ``nonce`` or in SQRL terminology SQRL nut
+    2. Strict enforcement that each nut can only be used at most once
+    3. Each non-initiating SQRL request should return to the client some information
+       about the initiating request such as whether IP address matches that of
+       the IP where SQRL transaction was initiated.
+
+    ``#3`` can easily be solved by encoding all necessary information in the nut value itself.
+    On subsequent requests, server can simply decode all the information from the nut.
+    That however will require nut value to grow pretty big. In addition this might also require
+    encryption so that nut value by itself is not revealing. That in turn requires some
+    rotating key management structure so that not all nuts are encrypted with same encryption key.
+    All of the above is possible however is not very elegant, but again, is still possible.
+
+    In addition, encrypting state in nut value does not hinder ``#1`` since some random bit
+    can also be part of the nut so ``#1`` is not an issue.
+
+    So far we dont need any state on the server. Each request can have its own nut value.
+
+    The problem however is with ``#2`` requirement. The only way for the server to guarantee
+    that nuts are never reused is to either keep a state of which nuts were used or keep state
+    of all currently available nuts. Since keeping state of all used nuts will infinitely grow,
+    probably the latter option is better. The default way of adding state in Django project
+    would of been to add a model. But at that point, if you are creating a model anyway,
+    might as well store everything in that model. As a benefit, it makes whole system
+    simpler by not requiring any fancy approaches to encrypting nuts and maintaining
+    rotated key encryption schedule of some sort.
+
     Attributes
     ----------
+    nonce : str
+        Base64 encoded value of a random nonce. This nonce is used to identify SQRL transaction.
+        It is regenerated for each SQRL communication within a single SQRL transaction.
+        Since this nonce is a one-time token, it allows for the server to prevent replay attacks.
+        This columns is a primary key of the table.
+    transaction_nonce : str
+        Base64 encoded random nonce used to identify a full SQRL transaction from start to finish.
+        Session key cannot be used since it is persistent across complete user visit which can
+        include multiple tabs/windows. This transaction id is regenerated for each new tab which
+        allows the client to identify when a particular SQRL transaction has completed
+        hence redirect user to more appropriate page.
     session_key : str
-        Session key
+        Session key of the session where SQRL transaction initiated which is used
+        to associate client session to SQRL transaction. This is important because SQRL
+        transaction can be completed on a different device which does not have access
+        to original user session.
+    is_transaction_complete : bool
+        Boolean indicating whether SQRL transaction has completed. It is used by the
+        :obj:`.views.SQRLStatusView` to return redirect URL to the user
+        when transaction has completed.
+    ip_address : str
+        IP address of the user who initiated SQRL transaction. This is where
+        initially SQRL link/qr code were generated.
+    timestamp : datetime
+        Last timestamp when nut was either created or modified. Used for puging purses
+        to remove expired nuts.
     """
     nonce = models.CharField(
         max_length=43, unique=True, db_index=True, primary_key=True,
         help_text='Single-use random nonce used to identify SQRL transaction. '
                   'This nonce is regenerated for each SQRL communication within '
                   'a single SQRL transaction. Since this nonce is a one-time token, '
-                  'it allows for the server to prevent replay attacks.'
+                  'it allows for the sviewerver to prevent replay attacks.'
     )
     transaction_nonce = models.CharField(
         max_length=43, unique=True, db_index=True,
@@ -77,7 +165,7 @@ class SQRLNut(models.Model):
                   'Session key cannot be used since it is persistent across '
                   'complete user visit which can include multiple tabs/windows. '
                   'This transaction id is regenerated for each new tab which '
-                  'allows the client to identity when a particular SQRL transaction '
+                  'allows the client to identify when a particular SQRL transaction '
                   'has completed hence redirect user to more appropriate page.'
     )
 
@@ -104,7 +192,7 @@ class SQRLNut(models.Model):
                   'Used for purging purposes.',
     )
 
-    objects = NutManager()
+    objects = SQRLNutManager()
 
     class Meta(object):
         # Explicitly define db_table for clearer table name
@@ -114,6 +202,12 @@ class SQRLNut(models.Model):
         return self.nonce
 
     def renew(self):
+        """
+        Renew instance of the nut.
+
+        This is done by deleting the nut from the db since nonce is a primary key.
+        Then nonce is regenerated and re-saved.
+        """
         self.delete()
         self.nonce = generate_randomness()
         self.save()
